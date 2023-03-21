40913cbb899833af37572171348bbd003aa334f7
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-03-20 22:47:31 -0400

    Add scroll animations to hero section

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Sina Nikmaram</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Your Apps Description" />
+        <meta name="description" content="Sina Nikmaram's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Navbar menuStatus={menuStatus} menu={handlers} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import styles from "../styles/hero.module.scss";
 import Button from "@mui/material/Button";
-import { TbCloudDownload } from "react-icons/tb";
+import { HiOutlineDocumentMagnifyingGlass } from "react-icons/hi2";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useEffect } from "react";
 
 const theme = createTheme({
   palette: {
@@ -13,6 +14,23 @@ const theme = createTheme({
 });
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("slideUp");
+        } else {
+          entry.target.classList.remove("slideUp");
+        }
+      });
+    });
+
+    let hiddenLeft = document.querySelectorAll(".hidden");
+    hiddenLeft.forEach((element) => {
+      observer.observe(element);
+    });
+  }, []);
+
   return (
     <>
       <section className={styles.hero}>
@@ -24,13 +42,17 @@ const Home: NextPage = () => {
           />
         </div>
 
-        <h4 id={styles.greeting}>Hi, my name is</h4>
-        <h1 id={styles.name}>Sina Nikmaram.</h1>
-        <p className={styles.sentence}>
+        <h4 id={styles.greeting} className="hidden">
+          Hi, my name is
+        </h4>
+        <h1 id={styles.name} className="hidden">
+          Sina Nikmaram.
+        </h1>
+        <p className={`${styles.sentence} ${styles.sentence1} hidden`}>
           I'm a software engineer specializing in the development of scalable
           solutions for complex, real-world problems.
         </p>
-        <p className={styles.sentence}>
+        <p className={`${styles.sentence} ${styles.sentence2} hidden`}>
           Currently, I'm focused on modernizing enterprise sustainability
           solutions with clients at IBM.
         </p>
@@ -39,12 +61,14 @@ const Home: NextPage = () => {
           <Button
             variant="outlined"
             color="success"
-            startIcon={<TbCloudDownload />}
+            startIcon={<HiOutlineDocumentMagnifyingGlass size={25} />}
+            className={`${styles.resume} hidden`}
             sx={{
               width: "15rem",
-              height: "4rem",
-              fontSize: "1.5rem",
-              margin: "1rem 0rem 5rem 0rem",
+              height: "5rem",
+              fontSize: "1.75rem",
+              margin: "0rem 0rem 5rem 0rem",
+              transitionDelay: "450ms !important",
             }}
           >
             Resume
