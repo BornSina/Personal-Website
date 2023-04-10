1102c6881db770226b7250025c47b6de1bd7c06d
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-04-10 14:24:57 -0400

    Add image to trait modal and update styling

--- src/components/TraitModal.tsx
+++ src/components/TraitModal.tsx
@@ -3,6 +3,7 @@ import { Modal } from "@mantine/core";
 import styles from "../styles/traitModal.module.scss";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Image from "next/image";
 
 interface TraitModalProps {
   title: string;
@@ -28,28 +29,59 @@ const TraitModal = ({ title, content, color, font }: TraitModalProps) => {
         opened={opened}
         onClose={close}
         title={<h1 style={{ fontSize: "2.5rem" }}>{title}</h1>}
+        size={"xl"}
         styles={{
-          header: {
-            height: "5rem",
-            backgroundColor: "rgb(39, 51, 75)",
-            color: "white",
-          },
           content: {
             backgroundColor: "rgb(39, 51, 75)",
             color: "white",
             fontSize: "1.3rem",
+            height: "40rem",
+          },
+          header: {
+            height: "5rem",
+            backgroundColor: "rgb(39, 51, 75)",
+            color: "white",
+            marginBottom: "1rem",
           },
           close: {
             width: "2.5rem",
             svg: {
               width: "3rem",
               height: "3rem",
+              color: "white",
+              transition: "color 0s ease-in-out !important",
+            },
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+            "&:active": {
+              backgroundColor: "transparent",
+              svg: {
+                color: "grey",
+              },
             },
           },
         }}
         centered
       >
-        {content}
+        <div
+          style={{
+            display: "flex",
+            width: `100%`,
+            paddingTop: "56.25%",
+            overflow: "hidden",
+            position: "relative",
+          }}
+        >
+          <Image src={`/images/${title}.webp`} alt={title} fill />
+        </div>
+        <div
+          style={{
+            paddingTop: "2rem",
+          }}
+        >
+          {content}
+        </div>
       </Modal>
 
       <ThemeProvider theme={theme}>
