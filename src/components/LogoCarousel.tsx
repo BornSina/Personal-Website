4187064aac4c301fa3ae28326fbb19ed33093a28
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-04-21 18:08:06 -0400

    Update logos and carousel styling

--- src/components/LogoCarousel.tsx
+++ src/components/LogoCarousel.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/logoCarousel.module.scss";
 
-interface techStackType {
+interface techStackRow {
   [name: string]: {
     height: string;
     width: string;
@@ -8,41 +8,51 @@ interface techStackType {
   };
 }
 
-let techStack: techStackType = {
+let techStack: techStackRow = {
   js: {
-    height: "6rem",
-    width: "6rem",
-    margin: "-.1rem 1.5rem 0rem 1rem",
+    height: "4.1rem",
+    width: "4.1rem",
+    margin: "-.1rem 1rem 0rem 0rem",
   },
   ts: {
-    height: "6.2rem",
-    width: "6.2rem",
-    margin: "0rem 1.25rem -.1rem 1.5rem",
+    height: "4.2rem",
+    width: "4.2rem",
+    margin: "0rem .75rem -.1rem 1rem",
   },
   python: {
-    height: "7.2rem",
-    width: "7.2rem",
+    height: "5.2rem",
+    width: "5.2rem",
     margin: "0rem 1.25rem -1rem 1.25rem",
   },
   go: {
-    height: "12.5rem",
-    width: "12.5rem",
-    margin: "0rem 1.75rem -3.4rem 0rem",
+    height: "10.5rem",
+    width: "10.5rem",
+    margin: "0rem 1.2rem -3.4rem -.25rem",
   },
   html: {
-    height: "7.5rem",
-    width: "7.5rem",
+    height: "5.5rem",
+    width: "5.5rem",
     margin: "0rem .25rem -.2rem 0rem",
   },
   css: {
-    height: "7.5rem",
-    width: "7.5rem",
-    margin: "0rem 0rem -.4rem .5rem",
+    height: "5.5rem",
+    width: "5.5rem",
+    margin: "0rem 0rem -.4rem .6rem",
   },
   sass: {
-    height: "8rem",
-    width: "8rem",
-    margin: "0rem 1.4rem -.9rem 1.75rem",
+    height: "6rem",
+    width: "6rem",
+    margin: "0rem 1.25rem -.9rem 1.5rem",
+  },
+  react: {
+    height: "6rem",
+    width: "6rem",
+    margin: "0rem 1.25rem -1rem .5rem",
+  },
+  mui: {
+    height: "6rem",
+    width: "6rem",
+    margin: "0rem 2rem -1rem .5rem",
   },
 };
 
@@ -59,7 +69,20 @@ const LogoCarousel = () => {
               margin: techStack[tech].margin || "0rem 2rem",
               width: techStack[tech].width,
               height: techStack[tech].height,
-              // border: "1px solid white",
+            }}
+          />
+        ))}
+      </div>
+      <div className={styles.logoSlide}>
+        {Object.keys(techStack).map((tech, i) => (
+          <img
+            src={`/assets/images/logos/${tech}.svg`}
+            alt={tech}
+            key={i}
+            style={{
+              margin: techStack[tech].margin || "0rem 2rem",
+              width: techStack[tech].width,
+              height: techStack[tech].height,
             }}
           />
         ))}
