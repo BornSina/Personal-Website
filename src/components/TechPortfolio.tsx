45c18eaa43a83e2edb38dab07dddd6003b9f8621
Sina Nikmaram <NikmaramCareer@gmail.com>
2023-04-21 19:23:21 -0400

    Refine logoCarousel order and styling

--- src/components/TechPortfolio.tsx
+++ src/components/TechPortfolio.tsx
@@ -11,50 +11,65 @@ interface techStackRow {
 }
 
 let techStack1: techStackRow = {
-  js: {
-    height: "4.1rem",
-    width: "4.1rem",
-    margin: "-.1rem 1rem 0rem 0rem",
-  },
-  ts: {
-    height: "4.2rem",
-    width: "4.2rem",
-    margin: "0rem .75rem -.1rem 1rem",
-  },
-  python: {
-    height: "5.2rem",
-    width: "5.2rem",
-    margin: "0rem 1.25rem -1rem 1.25rem",
-  },
-  go: {
-    height: "10.5rem",
-    width: "10.5rem",
-    margin: "0rem 1.2rem -3.4rem -.25rem",
-  },
   html: {
     height: "5.5rem",
     width: "5.5rem",
-    margin: "0rem .25rem -.2rem 0rem",
+    margin: "0rem 0.25rem -.2rem 0rem",
   },
   css: {
     height: "5.5rem",
     width: "5.5rem",
-    margin: "0rem 0rem -.4rem .6rem",
+    margin: "0rem 1.25rem -.4rem 0rem",
   },
-  sass: {
-    height: "6rem",
-    width: "6rem",
-    margin: "0rem 1.25rem -.9rem 1.5rem",
+  js: {
+    height: "4.1rem",
+    width: "4.1rem",
+    margin: "-.1rem 1.25rem 0rem 0rem",
+  },
+  ts: {
+    height: "4.2rem",
+    width: "4.2rem",
+    margin: "0rem 1rem -.1rem 1rem",
+  },
+  nodejs: {
+    height: "4.75rem",
+    width: "4.75rem",
+    margin: "0rem .9rem -.5rem 1.25rem",
   },
   react: {
     height: "6rem",
     width: "6rem",
-    margin: "0rem 1.25rem -1rem .5rem",
+    margin: "0rem .4rem -1rem .5rem",
+  },
+  sass: {
+    height: "6rem",
+    width: "6rem",
+    margin: "0rem .75rem -.9rem 1.5rem",
   },
   mui: {
     height: "6rem",
     width: "6rem",
-    margin: "0rem 2rem -1rem .5rem",
+    margin: "0rem 1.5rem -1.2rem 1rem",
+  },
+  tailwind: {
+    height: "7rem",
+    width: "7rem",
+    margin: "0rem .5rem -1.5rem 0rem",
+  },
+  bootstrap: {
+    height: "4.5rem",
+    width: "6.5rem",
+    margin: "0rem .75rem -.25rem .75rem",
+  },
+  python: {
+    height: "5.2rem",
+    width: "5.2rem",
+    margin: "0rem 1.25rem -1rem 1rem",
+  },
+  go: {
+    height: "10.5rem",
+    width: "10.5rem",
+    margin: "0rem 1.2rem -3.4rem -.4rem",
   },
 };
 
